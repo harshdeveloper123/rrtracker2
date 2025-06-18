9f8faa3edfae06510ec82d6baf20afff5b1c1fcb
harshdeveloper123 <140810197+harshdeveloper123@users.noreply.github.com>
2025-06-18 19:14:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const socketio = require("socket.io");
+const path = require("path"); // <-- Fix: Add this line
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public"))); // <-- also change `set` to `use`
+io.on("connection",function(socket){
+    socket.on("send location", function(data){
+        io.emit("receive-location",{id:socket.id, ...data})
+    })
+    // console.log("connected")
+    socket.on("disconnet",function(){
+        io.emit("user-dosconnected",socket.id)
+    })
+});
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+// server.listen(3000, () => {
+//     console.log("Server is running on http://localhost:3000");
+// });
+server.listen(3000)
